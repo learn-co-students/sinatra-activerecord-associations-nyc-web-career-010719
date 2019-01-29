0e18956bec86f9082d95461ef6c1a9591f3aeb7c
github username <clarencekwong7@gmail.com>
2019-01-29 17:42:07 -0500

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Cat.create(:name => "Maru", :age => 3, :breed => "Scottish Fold", :owner_id => 1)
+Cat.create(:name => "Hannah", :age => 2, :breed => "Tabby", :owner_id => 2)
+Cat.create(:name => "Patches", :age => 2, :breed => "Calico", :owner_id => 1)
+Owner.create(:name => "Sophie")
+Owner.create(:name => "Ann")
